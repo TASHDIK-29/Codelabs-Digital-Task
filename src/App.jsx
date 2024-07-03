d2752ca1f130b3a32affaaaf2179f984c22833f9
Tashdik <s2110477153@ru.ac.bd>
2024-07-03 13:49:53 +0600

    nav complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import { FiArrowUpRight } from "react-icons/fi";
+import navImg from "../../assets/Media/logo dark.png";
+
+const Navbar = () => {
+
+    const navLinks = <>
+        <li className="text-violet-950 text-base font-semibold mx-4">Home</li>
+        <li className="text-violet-950 text-base font-semibold mx-4">Service</li>
+        <li className="text-violet-950 text-base font-semibold mx-4">Blog</li>
+        <li className="text-violet-950 text-base font-semibold mx-4">About Us</li>
+
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {navLinks}
+                        <button className="btn btn-outline text-violet-950">Appointment <FiArrowUpRight className="text-lg" /></button>
+
+                    </ul>
+                </div>
+                <img src={navImg} alt="navImg" />
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLinks}
+                    
+                </ul>
+            </div>
+            <div className="hidden lg:flex navbar-end">
+                <button className="btn btn-outline text-violet-950">Appointment <FiArrowUpRight className="text-lg" /></button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
